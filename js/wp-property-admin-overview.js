0bcb49c590affa3bf175649fd62e48aabdce9034
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-14 14:05:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-property/trunk@275786 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wp-property-admin-overview.js
@@ -0,0 +1,19 @@
+/**
+ * WP-Property Admin Overview Scripts
+ *
+*/
+
+jQuery(document).ready(function() {
+
+	// Overview table thumbnail
+	jQuery(".fancybox").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+
+
+
+});
