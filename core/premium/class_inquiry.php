f6ed72e0dd7dd97513d6bf293f73e805b489746f
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-31 19:16:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-property/trunk@283341 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/premium/class_inquiry.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Name: Inquiry Management
+Class: class_inquiry
+Version: 2.0
+Description: A big map for property overview.
+*/
+
+?>
---
+++ core/premium/class_supermap.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Name: Supermap
+Class: class_supermap
+Version: 2.0
+Description: A big map for property overview.
+*/
+
+?>
