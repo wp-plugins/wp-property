230a58d0a630c6abc2206a79ad9dc5e5a2e8923a
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-31 13:32:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-property/trunk@283170 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/wp_properties.js
@@ -0,0 +1,11 @@
+	jQuery(document).ready(function() {
+
+		jQuery("a.fancybox_image").fancybox({
+			'transitionIn'	:	'elastic',
+			'transitionOut'	:	'elastic',
+			'speedIn'		:	600, 
+			'speedOut'		:	200, 
+			'overlayShow'	:	false
+		});
+
+	});
