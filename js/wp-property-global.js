8f14f6f11deda9222b839cae2a9b4c6ea41c58d9
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-02 15:07:43 +0000

    Added tag 1.38.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-property/tags/1.38.0@750042 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wp-property-global.js
@@ -0,0 +1,65 @@
+/**
+ * WP-Property Global Scripts
+ *
+ * This file is included on all front-end and specific back-end pages.
+ *
+*/
+
+/* Determine if page is loaded in frame. */
+if ( top === self ) {
+  //not in a frame
+} else {
+  //in a frame
+  if( typeof window.wpp === 'object' &&
+      typeof window.wpp.instance === 'object' &&
+      typeof window.wpp.instance.iframe_enabled !== 'undefined' && 
+      window.wpp.instance.iframe_enabled === true ) {
+    // ignore. Application allows to be used in iframe
+  } else {
+    top.location.href = document.location.href;
+  }
+}
+
+
+function wpp_format_currency( selector ) {
+
+  jQuery(selector).change(function() {
+    this_value = jQuery(this).val();
+    var val = jQuery().number_format( this_value.replace(/[^\d|\.]/g,'') );
+    jQuery(this).val( val );
+  });
+
+}
+
+
+function wpp_format_number( selector ) {
+
+  jQuery(selector).change(function() {
+    this_value = jQuery(this).val();
+    var val = jQuery().number_format( this_value.replace(/[^\d|\.]/g,''), {
+      numberOfDecimals:0,
+      decimalSeparator: '.',
+      thousandSeparator: ','
+    } );
+
+    if(val == 'NaN') {
+        val = '';
+      }
+
+    jQuery(this).val( val );
+  });
+
+}
+
+
+function wpp_add_commas( nStr ) {
+  nStr += '';
+  x = nStr.split('.');
+  x1 = x[0];
+  x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+  return x1 + x2;
+}
